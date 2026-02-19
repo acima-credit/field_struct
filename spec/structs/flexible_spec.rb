b6a18d0e6dc6aca052542eac7f8ed7d99110aff0
Adam Lauper <adam.lauper@acimacredit.com>
2026-02-19 16:00:24 -0700

    fixing rubocop violation

--- spec/structs/flexible_spec.rb
+++ spec/structs/flexible_spec.rb
@@ -54,7 +54,7 @@ end
 
 RSpec.describe FieldStruct::FlexibleExamples::User do
   describe 'class' do
-    it "responds to .mutable?" do
+    it 'responds to .mutable?' do
       expect(described_class).to respond_to :mutable?
       expect(described_class.mutable?).to be(false)
     end
--- spec/structs/strict_spec.rb
+++ spec/structs/strict_spec.rb
@@ -49,7 +49,7 @@ end
 
 RSpec.describe FieldStruct::StrictExamples::User do
   describe 'class' do
-    it "responds to .mutable?" do
+    it 'responds to .mutable?' do
       expect(described_class).to respond_to :mutable?
       expect(described_class.mutable?).to be(false)
     end
